2381a7fa8ea80cf0e5517e366e582f6ccfec154e
baidyprod <dima220920041@gmail.com>
2022-12-15 15:14:41 +0200

    V1

--- Homework14/library.py
+++ Homework14/library.py
@@ -1,3 +1,4 @@
+import csv
 from random import choice
 from abc import ABC, abstractmethod
 
@@ -34,22 +35,27 @@ class Game:
     ai = None
     res = None
     is_log_writer = None
+    winner = None
 
     def __init__(self, *, log_writer: bool):
         self.is_log_writer = log_writer
 
     def define_winner(self):
         '''
-        Method defines attribute res of Game class object
+        Defines attribute res of Game class object; defines the winner attribute
         '''
         if (self.human.figure, self.ai.figure) in rules.items():
             self.res = 'Congrats, You won!'
+            self.winner = 'Human'
         elif (self.ai.figure, self.human.figure) in rules.items():
             self.res = 'Congrats to AI, he won!'
+            self.winner = 'AI'
         elif self.human.figure == self.ai.figure:
             self.res = 'DRAW!'
+            self.winner = 'DRAW!'
         else:
             self.res = 'Did you write your figure properly? Replay!'
+            self.winner = "The game didn't happen"
 
     def text_msg(self):
         '''
@@ -67,18 +73,24 @@ class Game:
         Method logs full statistics of the game. When creating the object of class Game you should turn it on or off:
         Game(log_writer=True/False)
         '''
-        with open('game_results.txt', 'r') as file:
-            empty_lines = 0
-            for line in file:
-                if not line.strip():
-                    empty_lines += 1
-            game_count = empty_lines + 1
-
-        with open('game_results.txt', 'a') as file:
-            file.write(f'ROUND {game_count}\n')
-            file.write(f'Player choice is: {self.human.figure.capitalize()}\n')
-            file.write(f'AI choice is: {self.ai.figure.capitalize()}\n')
-            file.write(f'{self.res}\n\n')
+        with open('game_res.csv', 'r') as filename:
+            file = csv.DictReader(filename)
+            rounds = []
+            try:
+                for col in file:
+                    rounds.append(col['Round'])
+                round_count = int(rounds[-1]) + 1
+                write_header = False
+            except:
+                round_count = 1
+                write_header = True
+        with open('game_res.csv', 'a') as file:
+            header = ['Round', 'Human', 'AI', 'Result']
+            data = [round_count, self.human.figure.capitalize(), self.ai.figure.capitalize(), self.winner]
+            writer = csv.writer(file)
+            if write_header:
+                writer.writerow(header)
+            writer.writerow(data)
 
     def run(self):
         self.human = Human()
